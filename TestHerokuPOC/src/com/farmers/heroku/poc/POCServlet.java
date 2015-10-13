68f3a2834fa7081025134d4f43e8c1dba4cc45e6
parthadmishra <partha-sarathi.mishra@capgemini.com>
2015-10-13 11:19:09 -0700

    First Commit

---
+++ TestHerokuPOC/src/com/farmers/heroku/poc/POCServlet.java
@@ -0,0 +1,51 @@
+package com.farmers.heroku.poc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class POCServlet
+ */
+@WebServlet("/POCServlet")
+public class POCServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public POCServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("get request");
+		response.sendRedirect("Register.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Name : " + request.getParameter("name"));
+		System.out.println("Gender : " + request.getParameter("gender"));
+		System.out.println("Email : " + request.getParameter("email"));
+		System.out.println("Phone : " + request.getParameter("phone"));
+		System.out.println("City : " + request.getParameter("city"));
+
+		request.getRequestDispatcher("Details.jsp").forward(request, response);
+	}
+
+}
